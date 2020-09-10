3b02efae2f7057b837dca51b290287f8c7d1f9e5
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2020-09-10 21:35:49 +0300

    появилсось немного кэшрования

--- cash_LRU/cash_LRU.cpp
+++ cash_LRU/cash_LRU.cpp
@@ -7,39 +7,57 @@ struct node_t
 {
 	int request;
 	int year;
-	int hash_key;
 };
 
 class cash
 {
 		std::list<node_t> cash_list;
 		std::unordered_map<int , std::list<node_t>::iterator> cash_table;
-		int p;
-		int counter;
+		int max;
 	public:
 		cash(int MAX);
 		void look_up(int request);
+		void print_cash();
 };
 
 cash::cash(int MAX)
 {
 	std::list<node_t> cash_list;
 	std::unordered_map<int , std::list<node_t>::iterator> cash_table;
-	p = MAX;
-	int counter = 0;
+	max = MAX;
 };
 
 void cash::look_up(int request)
 {
-	node_t A;
-	A.request = request;
-	A.year = 1;
-	A.hash_key = request;
-	cash_list.push_front(A);
-	std::list<node_t>::iterator it = cash_list.begin();
-	cash_table[counter] = it;
-	/*for(node_t n : cash_list)
-		std::cout << n.request << '\n';*/
+	node_t put;
+	if (cash_table.find(request) != cash_table.end())
+		(cash_table[request] -> year)++;
+		//shift_down
+	else
+	{
+		put.request = request;
+		put.year = 1;
+		if (cash_list.size() != max)
+		{
+			cash_list.push_front(put);
+			std::list<node_t>::iterator it = cash_list.begin();
+			cash_table[put.request] = it;
+		};
+		/*else
+		{
+			cash_table.erase(cash_table.find(request));
+			cash_list.pop_front();
+			cash_list.push_front(put);
+			std::list<node_t>::iterator it = cash_list.begin();
+			cash_table[put.request] = it;
+		};*/
+	};
+};
+
+void cash::print_cash()
+{
+	for (node_t n : cash_list)
+		std::cout << n.request << '-' << n.year <<'\n';
 };
 
 int main()
@@ -47,33 +65,10 @@ int main()
 	cash LFU(10);
 	for (int i = 0 ; i < 10 ; i++)
 		LFU.look_up(i);
-	/*std::list<node_t> first_list;
-	node_t A[10];
-	for (int i = 0 ; i < 10 ; i++)
-	{
-		A[i].request = i;
-		A[i].year = i;
-	};
-	for (int i = 0 ; i < 10 ; i++)
-		first_list.push_back(A[i]);
-	for (node_t n : first_list) 
-        std::cout << n.year << '\n';
-	std::cout << '\n';
-	A[1].year = 1000;
-	A[2].year = 2000;
-	A[3].year = 3000;
-	first_list.push_back(A[1]);
-	first_list.push_front(A[2]);
-	std::list<node_t>::iterator it = first_list.begin();
-	std::advance(it , 5);
-	first_list.insert(it , A[3]);
-	unordered_map<int , node_t> first_map;
-	first_map[0] = A[3];
-	for (node_t n : first_list) 
-        std::cout << n.year << '\n';
-	unordered_map<int , node_t> first_map;
-	first_map[0] = A[3];
-	std::unordered_map<int , node_t>::iterator = first_map.find(0);*/
+	for (int i = 0 ; i < 9 ; i++)
+		LFU.look_up(i);
+	LFU.look_up(7);
+	LFU.print_cash();
 	return 0;
 };
 	
