a776e8f98b4ee7071df494f2c2ab7d4e3f29f931
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2020-10-16 19:43:36 +0300

    научились транспонироввать матрицу

--- MATRIX/matrix.cpp
+++ MATRIX/matrix.cpp
@@ -10,9 +10,15 @@ class matrix
 	public:
 		matrix(int num_str , int num_col);
 		matrix(int num);
-		int fill_M(/*FILE* in*/);
+		~matrix();
+		int count_col() {return num_col;};
+		int count_str() {return num_str;};
+		int m_type() {return type;};
+		int get_val(int str , int col) {return mrx[col][str];};
+		int fill_M();
 		int print_M();
-		//matrix transpotion (matrix A);
+		void put_M(int val , int str , int col) {mrx[col][str] = val;};
+		matrix transpotion ();
 		//int determinate(matrix A);
 		//matrix reverse(matrix A);
 };
@@ -39,17 +45,16 @@ matrix::matrix(int num)
 	type = 1;
 };
 
-int matrix::fill_M(/*FILE* in*/)
+matrix::~matrix()
 {
-	/*int* per_mrx = new int (count);
-	fread(per_mrx , sizeof(int) , count , in);
-	for (int i = 0 ; i < num_str ; ++i)
-		for (int j = 0 ; i < num_col ; ++i)
-		{
-			if ((i + j) >= count)
-				return 0;
-			mrx[i][j] = per_mrx[i + j];
-		}*/
+	for (int i = 0 ; i < num_col ; ++i)
+		delete[] mrx[i];
+	delete[] mrx;
+	std::cout << "deleted" << "\n";
+}
+
+int matrix::fill_M()
+{
 	for (int i = 0 ; i < num_str ; i++)
 	{
 		for (int j = 0 ; j < num_col ; j++)
@@ -76,19 +81,22 @@ int matrix::print_M()
 }
 
 
-
-
-
-
-
-
+matrix matrix::transpotion()
+{
+	int num_c = count_str();
+	int num_s = count_col();
+	matrix A_trans(num_s , num_c);
+	for (int i = 0 ; i < num_s ; i++)
+		for (int j = 0 ; j < num_c ; j++)
+			A_trans.put_M(get_val(j , i) , i , j);
+	return A_trans;
+}
 
 int main()
 {
-	matrix test(2);
+	matrix test(2 , 3);
 	int res = test.fill_M();
-	printf("%d\n" , res);
-	res = test.print_M();
-	printf("%d\n" , res);
+	matrix test_t = test.transpotion();
+	test_t.print_M();
 	return 0;
 }
