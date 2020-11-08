295853899090b2088c842a7bfa3553d8be9ee8dc
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2020-11-09 01:07:08 +0300

    вроде работает пока без тестов

--- MATRIX/matrix.cpp
+++ MATRIX/matrix.cpp
@@ -2,6 +2,13 @@
 #include <iomanip>
 #include <assert.h>
 
+#define DBL_EPSILON 0.0000001
+
+
+
+namespace Matrix_Ilab{
+
+
 template<typename T>
 class matrix
 {
@@ -13,33 +20,45 @@ class matrix
 	public:
 		matrix<T>(int num_str , int num_col);
 		matrix<T>(int num);
+		//matrix<T>(int num_str , int num_col , T val = T{});
+		template <typename It>
+		matrix<T>(int cols, int rows, It start, It fin);
 		matrix(const matrix& rhs);
+		~matrix();
 		matrix<T>& operator=(const matrix& rhs);
 		matrix<T>& operator+(const matrix& rhs);
 		matrix<T>& operator-(const matrix& rhs);
 		matrix<T>& operator*(const matrix& rhs);
+		matrix<T>& operator+=(const matrix& rhs);
+		matrix<T>& operator-=(const matrix& rhs);
+		matrix<T>& operator*=(const matrix& rhs);
+		bool operator==(const matrix& rhs);
+		bool operator!=(const matrix& rhs);
 		matrix<T> operator[](const matrix& rhs);
 		int* operator[](const int rhs);
-		~matrix();
 		int count_col() const {return num_col;};
 		int count_str() const {return num_str;};
 		int m_type() {return type;};
 		T get_val(int str , int col) const {return mrx[str][col];};
-		int fill_M();
-		int print_M();
 		void put_M(int str , int col , T val) const {mrx[str][col] = val;};
-		matrix<T> transpotion ();
-		T minor_T(int str , int col);
-		T determinate();
 		void swap_rows(int row_1 , int row_2);
 		void sub_rows(int row_1 , int row_2);
 		void row_k(int row , float k);
-		//matrix<T> reverse(matrix A);
+		void fill_M();
+		void fill_M_rand();
+		void print_M();
 		void negate();
+		matrix<T> transpotion ();
+		T determinate();
+		//T minor_T(int str , int col);
+		//matrix<T> reverse(matrix A);
 };
 
 template<typename T>
 void swap(T* per_1 , T* per_2);
+
+template<typename T>
+void iszero(T* per);
 
 template<typename T>
 matrix<T>& matrix<T>::operator=(const matrix& rhs)
@@ -53,40 +72,37 @@ matrix<T>& matrix<T>::operator=(const matrix& rhs)
 	count = rhs.count_col() * rhs.count_str();
 	type = rhs.type;
 	return *this;
-};
+}
 
 template<typename T>
 matrix<T>& matrix<T>::operator+(const matrix& rhs)
 {
 	assert(num_col == rhs.count_col());
 	assert(num_str == rhs.count_str());
-	for (int i = 0 ; i < num_str ; i++)
-		for (int j = 0 ; j < num_col ; j++)
-			mrx[i][j] += rhs.get_val(i , j);
-	return *this;
-};
+	matrix<T> buf = *this;
+	buf += rhs;
+	return buf;
+}
 
 template<typename T>
 matrix<T>& matrix<T>::operator-(const matrix& rhs)
 {
 	assert(num_col == rhs.count_col());
 	assert(num_str == rhs.count_str());
-	for (int i = 0 ; i < num_str ; i++)
-		for (int j = 0 ; j < num_col ; j++)
-			mrx[i][j] -= rhs.get_val(i , j);
-	return *this;
-};
+	matrix<T> buf = *this;
+	buf -= rhs;
+	return buf;
+}
 
 template<typename T>
 matrix<T>& matrix<T>::operator*(const matrix& rhs)
 {
 	assert(num_col == rhs.count_col());
 	assert(num_str == rhs.count_str());
-	for (int i = 0 ; i < num_str ; i++)
-		for (int j = 0 ; j < num_col ; j++)
-			mrx[i][j] = mrx[i][j] * rhs.get_val(i , j);
-	return *this;
-};
+	matrix<T> buf = *this;
+	buf *= rhs;
+	return buf;
+}
 
 template<typename T>
 int* matrix<T>::operator[](const int rhs)
@@ -97,7 +113,7 @@ int* matrix<T>::operator[](const int rhs)
 	for (int i = 0 ; i < num_col ; i++)
 		str[i] = mrx[rhs][i];
 	return str;
-};
+}
 
 template<typename T>
 matrix<T> matrix<T>::operator[](const matrix& rhs)
@@ -117,8 +133,62 @@ matrix<T> matrix<T>::operator[](const matrix& rhs)
 		};
 	};
 	return result;
-};
+}
 
+template<typename T>
+matrix<T>& matrix<T>::operator+=(const matrix<T>& rhs)
+{
+	assert(num_col == rhs.count_col());
+	assert(num_str == rhs.count_str());
+	for (int i = 0 ; i < num_str ; ++i)
+		for (int j = 0 ; j < num_col ; ++j)
+			mrx[i][j] += rhs.get_val(i , j);
+	return *this;
+}
+
+template<typename T>
+matrix<T>& matrix<T>::operator-=(const matrix<T>& rhs)
+{
+	assert(num_col == rhs.count_col());
+	assert(num_str == rhs.count_str());
+	for (int i = 0 ; i < num_str ; ++i)
+		for (int j = 0 ; j < num_col ; ++j)
+			mrx[i][j] -= rhs.get_val(i , j);
+	return *this;
+}
+
+template<typename T>
+matrix<T>& matrix<T>::operator*=(const matrix<T>& rhs)
+{
+	assert(num_col == rhs.count_col());
+	assert(num_str == rhs.count_str());
+	for (int i = 0 ; i < num_str ; ++i)
+		for (int j = 0 ; j < num_col ; ++j)
+			mrx[i][j] *= rhs.get_val(i , j);
+	return *this;
+}
+
+template <typename T>
+bool matrix<T>::operator==(const matrix<T>& rhs)
+{
+	assert(num_col == rhs.count_col());
+	assert(num_str == rhs.count_str());
+	for (int i = 0 ; i < num_str ; ++i)
+		for (int j = 0 ; j < num_col ; ++j)
+			if (mrx[i][j] != rhs.get_val(i , j))
+				return 0;
+	return 1;
+}
+
+template <typename T>
+bool matrix<T>::operator!=(const matrix<T>& rhs)
+{
+	assert(num_col == rhs.count_col());
+	assert(num_str == rhs.count_str());
+	if (*this == rhs)
+		return 0;
+	return 1;
+}
 
 template<typename T>
 matrix<T>::matrix(const matrix& rhs)
@@ -133,7 +203,7 @@ matrix<T>::matrix(const matrix& rhs)
 	num_str = rhs.count_str();
 	count = rhs.count_col() * rhs.count_str();
 	type = rhs.type;
-};
+}
 
 template<typename T>
 matrix<T>::matrix(int num_s , int num_c)
@@ -145,7 +215,7 @@ matrix<T>::matrix(int num_s , int num_c)
 	num_str = num_s;
 	count = num_col * num_str;
 	(num_col == num_str) ? type = 1 : type = 0;
-};
+}
 
 template<typename T>
 matrix<T>::matrix(int num)
@@ -157,7 +227,26 @@ matrix<T>::matrix(int num)
 	num_str = num;
 	count = num_col * num_str;
 	type = 1;
-};
+}
+
+template <typename T>
+template <typename It>
+matrix<T>::matrix(int num_s , int num_c , It start , It fin)
+{
+	mrx = new T* [num_s];
+	for (int j = 0 ; j < num_s ; ++j)
+		mrx[j] = new T [num_c];
+	num_col = num_c;
+	num_str = num_s;
+	count = num_col * num_str;
+	(num_col == num_str) ? type = 1 : type = 0;
+	for (int i = 0 ; i < num_str ; ++i)
+		for (int j = 0 ; j < num_col ; ++j)
+		{
+			mrx[i][j] = *start;
+			start++;
+		};
+}
 
 template<typename T>
 matrix<T>::~matrix()
@@ -165,19 +254,28 @@ matrix<T>::~matrix()
 	for (int i = 0 ; i < num_col ; ++i)
 		delete[] mrx[i];
 	delete[] mrx;
-};
+}
 
 template<typename T>
-int matrix<T>::fill_M()
+void matrix<T>::fill_M()
+{
+	for (int i = 0 ; i < num_str ; i++)
+	{
+		for (int j = 0 ; j < num_col ; j++)
+			std::cin >> mrx[i][j];
+	}
+}
+
+template<typename T>
+void matrix<T>::fill_M_rand()
 {
 	for (int i = 0 ; i < num_str ; i++)
 		for (int j = 0 ; j < num_col ; j++)
-			std::cin >> mrx[i][j];
-	return 1;
-};
+			mrx[i][j] = ((rand() % 10) - 5);
+}
 
 template<typename T>
-int matrix<T>::print_M()
+void matrix<T>::print_M()
 {
 	std::cout << "count = " << count << "\n";
 	std::cout << "num_col = " << num_col << "\n";
@@ -190,8 +288,7 @@ int matrix<T>::print_M()
 		std::cout << "\n";
 	}
 	std::cout << "\n";
-	return 1;
-};
+}
 
 template<typename T>
 matrix<T> matrix<T>::transpotion()
@@ -203,7 +300,7 @@ matrix<T> matrix<T>::transpotion()
 		for (int j = 0 ; j < num_c ; j++)
 			A_trans.put_M(i , j , get_val(j , i));
 	return A_trans;
-};
+}
 
 template<typename T>
 void matrix<T>::negate()
@@ -211,7 +308,7 @@ void matrix<T>::negate()
 	for (int i = 0 ; i < num_str ; i++)
 		for (int j = 0 ; j < num_col ; j++)
 			mrx[i][j] = (-1) * mrx[i][j];
-};
+}
 
 template<typename T>
 T matrix<T>::determinate()
@@ -222,9 +319,22 @@ T matrix<T>::determinate()
 	for (int i = 0 ; i < num_str ; i++)
 	{
 		T max = copy.get_val(i , i);
+		int point = i;
 		int num_s = i;
+		while (point < num_str)
+		{
+			if (copy.get_val(point , i) != 0)
+			{
+				max = copy.get_val(point , i);
+				num_s = point;
+				break;
+			};
+			point++;
+		};
+		if (max == 0)
+			return 0;
 		for (int j = i ; j < num_str ; j++)
-			if (copy.get_val(j , i) > max)
+			if ((abs(copy.get_val(j , i)) > abs(max)) && (copy.get_val(j , i) != 0))
 			{
 				max = copy.get_val(j , i);
 				num_s = j;
@@ -233,25 +343,24 @@ T matrix<T>::determinate()
 		det = det * copy.get_val(i , i);
 		for (int j = i ; j < num_str ; ++j)
 		{
-			if (j == i)
+			if ((j == i) || (copy.get_val(j , i) == 0))
 				continue;
-			float per = copy.get_val(j , i) / copy.get_val(i , i);
+			T per = copy.get_val(j , i) / copy.get_val(i , i);
+			T* row = new T[num_col - i];
+			for (int l = 0 ; l < num_col - i ; l++)
+				row[l] = copy.get_val(i , l + i);
 			copy.row_k(i , per);
+			copy.print_M();
 			copy.sub_rows(j , i);
-			per = 1 / per;
-			copy.row_k(i , per);
+			copy.print_M();
+			for (int l = 0 ; l < num_col - i ; l++)
+				copy.put_M(i , l + i , row[l]);
+			delete[] row;
 		};
 	};
+	iszero(&det);
 	return det;
-};
-
-float test_det(int size)
-{
-	matrix<float> test(size);
-	test.fill_M();
-	float det = test.determinate();
-	return det;
-};
+}
 
 template<typename T>
 void matrix<T>::swap_rows(int row_1 , int row_2)
@@ -260,7 +369,7 @@ void matrix<T>::swap_rows(int row_1 , int row_2)
 	assert(row_2 < num_str);
 	for (int i = 0 ; i < num_col ; i++)
 		swap(&mrx[row_1][i] , &mrx[row_2][i]);
-};
+}
 
 template<typename T>
 void swap(T* per_1 , T* per_2)
@@ -268,7 +377,7 @@ void swap(T* per_1 , T* per_2)
 	T per = *per_1;
 	*per_1 = *per_2;
 	*per_2 = per;
-};
+}
 
 template<typename T>
 void matrix<T>::sub_rows(int row_1 , int row_2)
@@ -277,7 +386,7 @@ void matrix<T>::sub_rows(int row_1 , int row_2)
 	assert(row_2 < num_str);
 	for (int i = 0 ; i < num_col ; i++)
 		mrx[row_1][i] = mrx[row_1][i] - mrx[row_2][i];
-};
+}
 
 template<typename T>
 void matrix<T>::row_k(int row , float k)
@@ -285,12 +394,18 @@ void matrix<T>::row_k(int row , float k)
 	assert(row < num_str);
 	for (int i = 0 ; i < num_col ; i++)
 		mrx[row][i] = mrx[row][i] * k;
-};
+}
 
-/*T matrix::minor_T(int str , int col)
+template<typename T>
+void iszero(T* per)
 {
-	
-}*/
+	if (abs(*per) <= DBL_EPSILON)
+		*per = 0;
+}
+
+}
+
+using namespace Matrix_Ilab;
 
 int main()
 {
