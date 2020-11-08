c30d1433946b1f52e8928caebee2ba1397f1d30f
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2020-11-08 18:40:15 +0300

    add function of determinant

--- MATRIX/matrix.cpp
+++ MATRIX/matrix.cpp
@@ -29,11 +29,17 @@ class matrix
 		int print_M();
 		void put_M(int str , int col , T val) const {mrx[str][col] = val;};
 		matrix<T> transpotion ();
-		//T minor_T(int str , int col);
+		T minor_T(int str , int col);
 		T determinate();
+		void swap_rows(int row_1 , int row_2);
+		void sub_rows(int row_1 , int row_2);
+		void row_k(int row , float k);
 		//matrix<T> reverse(matrix A);
 		void negate();
 };
+
+template<typename T>
+void swap(T* per_1 , T* per_2);
 
 template<typename T>
 matrix<T>& matrix<T>::operator=(const matrix& rhs)
@@ -205,43 +211,86 @@ void matrix<T>::negate()
 	for (int i = 0 ; i < num_str ; i++)
 		for (int j = 0 ; j < num_col ; j++)
 			mrx[i][j] = (-1) * mrx[i][j];
-}
+};
 
 template<typename T>
 T matrix<T>::determinate()
 {
 	assert(num_str == num_col);
-	if (mrx[0][0] == 0)
-		return 0;
-	matrix<T> L(num_str);
-	matrix<T> U(num_str);
-	T sum1 = 0;
-	T sum2 = 0;
+	matrix copy = *this;
 	T det = 1;
-	for (int j = 0 ; j < num_str ; j++)
-	{
-		U.put_M(0 , j , mrx[0][j]);
-		L.put_M(j , 0 , mrx[j][0] / U.get_val(0 , 0));
-	};
-	for (int i = 1 ; i < num_str ; i++)
-		for (int j = i ; j < num_str ; j++)
-		{
-			for (int k = 0 ; k < i ; k++)
-			{
-				sum1 += L.get_val(i , k) * U.get_val(k , j);
-				sum2 += L.get_val(j , k) * U.get_val(k , i);
-			};
-			U.put_M(i , j , mrx[i][j] - sum1);
-			if (U.get_val(i , j) == 0)
-				return 0;
-			L.put_M(j , i , (mrx[j][i] - sum2) / U.get_val(i , i));
-			sum1 = 0;
-			sum2 = 0;
-		};
 	for (int i = 0 ; i < num_str ; i++)
-		det = det * U.get_val(i , i);
+	{
+		T max = copy.get_val(i , i);
+		int num_s = i;
+		for (int j = i ; j < num_str ; j++)
+			if (copy.get_val(j , i) > max)
+			{
+				max = copy.get_val(j , i);
+				num_s = j;
+			};
+		copy.swap_rows(num_s , i);
+		det = det * copy.get_val(i , i);
+		for (int j = i ; j < num_str ; ++j)
+		{
+			if (j == i)
+				continue;
+			float per = copy.get_val(j , i) / copy.get_val(i , i);
+			copy.row_k(i , per);
+			copy.sub_rows(j , i);
+			per = 1 / per;
+			copy.row_k(i , per);
+		};
+	};
 	return det;
 };
+
+float test_det(int size)
+{
+	matrix<float> test(size);
+	test.fill_M();
+	float det = test.determinate();
+	return det;
+};
+
+template<typename T>
+void matrix<T>::swap_rows(int row_1 , int row_2)
+{
+	assert(row_1 < num_str);
+	assert(row_2 < num_str);
+	for (int i = 0 ; i < num_col ; i++)
+		swap(&mrx[row_1][i] , &mrx[row_2][i]);
+};
+
+template<typename T>
+void swap(T* per_1 , T* per_2)
+{
+	T per = *per_1;
+	*per_1 = *per_2;
+	*per_2 = per;
+};
+
+template<typename T>
+void matrix<T>::sub_rows(int row_1 , int row_2)
+{
+	assert(row_1 < num_str);
+	assert(row_2 < num_str);
+	for (int i = 0 ; i < num_col ; i++)
+		mrx[row_1][i] = mrx[row_1][i] - mrx[row_2][i];
+};
+
+template<typename T>
+void matrix<T>::row_k(int row , float k)
+{
+	assert(row < num_str);
+	for (int i = 0 ; i < num_col ; i++)
+		mrx[row][i] = mrx[row][i] * k;
+};
+
+/*T matrix::minor_T(int str , int col)
+{
+	
+}*/
 
 int main()
 {
@@ -250,6 +299,6 @@ int main()
 	matrix<float> test_1(size);
 	test_1.fill_M();
 	float det = test_1.determinate();
-	std::cout << det << '\n';
+	std::cout << det << "\n";
 	return 0;
-}
+};
