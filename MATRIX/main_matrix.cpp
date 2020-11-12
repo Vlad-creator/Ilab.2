2e6301417ffea03c3f2413e8fa05db3363de56ea
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2020-11-12 15:56:23 +0300

    Now Matrix<int>

--- MATRIX/main_matrix.cpp
+++ MATRIX/main_matrix.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	int size = 0;
 	std::cin >> size;
-	matrix<long double> test(size);
+	matrix<int> test(size);
 	test.fill_M();
 	long double det = test.determinate();
 	std::cout << det << std::endl;
