d5d3c31c95d48b7d5a67fc516aa604dd774f7219
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-02-05 12:54:45 +0300

    something like main

--- Triangles/main.cpp
+++ Triangles/main.cpp
@@ -1,17 +1,13 @@
 #include "Triangle.h"
+#include "cross_tr.h"
 
 int main()
 {
-	point p1(0 , 0 , 0);
-	point p2(1 , 0 , 0);
-	point p3(0 , 1 , 0);
-	point p4(5 , 0 , 0);
-	point p5(3 , 0 , 0);
-	point p6(0 , 3 , 0);
-	triangle t1(p1 , p2 , p3);
-	triangle t2(p4 , p5 , p6);
-
-	bool res = is_cross(t1 , t2);
-	std::cout << res << std::endl;
+	int N = 0;
+	std::cin >> N;
+	triangle TRs[10000];
+	for (int i = 0 ; i < N ; ++i)
+		std::cin >> TRs[i];
+	TRs[1].print();
 	return 0;
 }
