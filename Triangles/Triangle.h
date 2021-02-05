a4fe22ebc5d4092d3140407620fb15f21e30b6bf
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-02-05 12:28:57 +0300

    fixed some bugs remove main function

--- Triangles/Triangle.h
+++ Triangles/Triangle.h
@@ -55,7 +55,8 @@ struct triangle
 	point p1_ , p2_ , p3_;
 
 	void print() const;
-	point find_norm();
+	point find_norm_tr();
+	point find_norm_ln(point line);
 	triangle(point p1 , point p2 , point p3);
 };
 
@@ -66,12 +67,19 @@ triangle::triangle(point p1 , point p2 , point p3)
 	p3_ = p3;
 }
 
-point triangle::find_norm()
+point triangle::find_norm_tr()
 {
 	point vec1 = make_vec(p1_ , p2_);
 	point vec2 = make_vec(p1_ , p3_);
-	point normal = vec_product(vec1 , vec2);
-	return normal;
+	point norm_tr = vec_product(vec1 , vec2);
+	return norm_tr;
+}
+
+point triangle::find_norm_ln(point line)
+{
+	point norm_tr = find_norm_tr();
+	point norm_ln = vec_product(norm_tr , line);
+	return norm_ln;
 }
 
 void triangle::print() const
@@ -87,70 +95,6 @@ void triangle::print() const
 std::istream& operator>>(std::istream& stream, point& a) {
 	stream >> a.x_ >> a.y_ >> a.z_;
 	return stream;
-}
-
-bool proect_cross(triangle t1 , triangle t2 , point line)
-{
-	int counter = 0;
-	std::vector<float> lines_t1 = {};
-	std::vector<float> lines_t2 = {};
-	lines_t1.push_back(dot_product(t1.p1_ , line));
-	lines_t1.push_back(dot_product(t1.p2_ , line));
-	lines_t1.push_back(dot_product(t1.p3_ , line));
-	lines_t2.push_back(dot_product(t2.p1_ , line));
-	lines_t2.push_back(dot_product(t2.p2_ , line));
-	lines_t2.push_back(dot_product(t2.p3_ , line));
-
-	for (int i = 0 ; i < 3 ; ++i)
-		for (int j = 0 ; j < 3 ; ++j)
-			if (lines_t1[i] < lines_t2[j])
-				counter++;
-	if (counter == 9)
-		return false;
-
-	counter = 0;
-
-	for (int i = 0 ; i < 3 ; ++i)
-		for (int j = 0 ; j < 3 ; ++j)
-			if (lines_t1[i] > lines_t2[j])
-				counter++;
-	if (counter == 9)
-		return false;
-
-	return true;
-}
-
-bool is_cross(triangle t1 , triangle t2)
-{
-	bool res = true;
-	std::vector<point> lines_t1 = {};
-	std::vector<point> lines_t2 = {};
-	lines_t1.push_back(make_vec(t1.p1_ , t1.p2_));
-	lines_t1.push_back(make_vec(t1.p1_ , t1.p3_));
-	lines_t1.push_back(make_vec(t1.p2_ , t1.p3_));
-	lines_t2.push_back(make_vec(t2.p1_ , t2.p2_));
-	lines_t2.push_back(make_vec(t2.p1_ , t2.p3_));
-	lines_t2.push_back(make_vec(t2.p2_ , t2.p3_));
-
-	point norm_t1 = t1.find_norm();
-	res = proect_cross(t1 , t2 , norm_t1);
-		if (res == false)
-			return res;
-
-	point norm_t2 = t2.find_norm();
-	res = proect_cross(t1 , t2 , norm_t2);
-		if (res == false)
-			return res;
-
-	for (int i = 0 ; i < 3; ++i)
-		for (int j = 0 ; j < 3 ; ++j)
-		{
-			point line = vec_product(lines_t1[i] , lines_t2[j]);
-			res = proect_cross(t1 , t2 , line);
-			if (res == false)
-				return res;
-		};
-	return res;
 }
 
 
