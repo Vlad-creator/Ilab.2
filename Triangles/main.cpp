b1367f575f5e22a7516f10dff70dc6167fb6e3f1
Vlad Loznovenko <loznovenko.vi@phystech.edu>
2021-02-08 00:14:15 +0300

    something like main

--- Triangles/main.cpp
+++ Triangles/main.cpp
@@ -1,13 +1,30 @@
+#include <vector>
+#include <iostream>
+#include <float.h>
+#include <math.h>
 #include "Triangle.h"
 #include "cross_tr.h"
+#include "octa_tree.h"
 
 int main()
 {
 	int N = 0;
+	triangle per;
 	std::cin >> N;
-	triangle TRs[10000];
+	std::vector<triangle> TRs;
 	for (int i = 0 ; i < N ; ++i)
-		std::cin >> TRs[i];
-	TRs[1].print();
+	{
+		std::cin >> per;
+		TRs.push_back(per);
+	}
+
+	int res = 0;
+
+	//std::cout << is_cross(TRs[0] , TRs[1]);
+
+	octa_tree head(TRs);
+	head.make_tree(&res);
+
+	std::cout << res << std::endl;
 	return 0;
 }
